e14f2fe64d8e4d97af7c1ac4f945745dd358c19a
Andrii Dubovyk <and.dubovyk@gmail.com>
2023-04-28 10:10:29 +0300

    Hide password

--- composeApp/src/commonMain/kotlin/zlagoda/ukma/edu/ua/screens/login/ui/LoginViewAuth.kt
+++ composeApp/src/commonMain/kotlin/zlagoda/ukma/edu/ua/screens/login/ui/LoginViewAuth.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import zlagoda.ukma.edu.ua.screens.login.viewmodel.LoginEvent
 import zlagoda.ukma.edu.ua.screens.login.viewmodel.LoginState
@@ -30,10 +33,12 @@ internal fun LoginViewAuth(state: LoginState.Auth, onEvent: (LoginEvent) -> Unit
             modifier = Modifier.fillMaxWidth().padding(16.dp)
         )
 
+        var passwordVisible by remember { mutableStateOf(false) }
         OutlinedTextField(
             value = state.password,
             onValueChange = { password -> onEvent(LoginEvent.SetPassword(password)) },
             label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth().padding(16.dp)
         )
 
